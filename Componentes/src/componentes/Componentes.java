86b311ec662fe2b838a079afb3ca41f248819f4a
[Alejandro-15] <[alejandrocastillo44@aragon.unam.mx]>
2021-06-24 07:49:34 -0500

    Tarea 2

--- Componentes/src/componentes/Componentes.java
+++ Componentes/src/componentes/Componentes.java
@@ -23,41 +23,41 @@ public class Componentes {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        CPU cpu1 = new CPU("HP","Notebook",250);
-        Computadora computadora1 = new Computadora("HP","Notebook","HP","LED","Atheris","Omen","Acer","LED",7, (int) 2.5,"alambrico",17,6);
-        Monitor monitor1 = new Monitor("Acer","LED",14);
-        Mouse mouse1= new Mouse("Atheris","Omen","Alambrico");
-        Teclado teclado1 = new Teclado("HP","TM",15,17);
-        System.out.println("Marca de CPU es:"+cpu1.getMarca());
-        System.out.println("Modelo de CPU es:"+cpu1.getModelo());
-        System.out.println("velocidad de CPU es:"+cpu1.getVelocidadProcesador());
+        CPU cpu1 = new CPU("intel","i9 9900K",5);
+        Computadora computadora1 = new Computadora("HP","Pavilion gamer","Yeyian","Flare serie 2000","Logitech","G203","ASUS","VP229HE",22, (int) 5,"alambrico",105,12);
+        Monitor monitor1 = new Monitor("ASUS","VP229HE",22);
+        Mouse mouse1= new Mouse("Logitech","G203","Alambrico");
+        Teclado teclado1 = new Teclado("Yeyian","Flare serie 2000",105,12);
+        System.out.println("Marca de Procesador:"+cpu1.getMarca());
+        System.out.println("Modelo de Procesador:"+cpu1.getModelo());
+        System.out.println("velocidad de Procesador:"+cpu1.getVelocidadProcesador());
         
-        System.out.println("Marca de Computadora es:"+computadora1.getMarca());
-        System.out.println("Modelo de Computadora es:"+computadora1.getMarca());
-        System.out.println("Marca de teclado de Computadora es:"+computadora1.getMarcaTeclado());
-        System.out.println("Modelo de teclado de Computadora es:"+computadora1.getModeloTeclado());
-        System.out.println("Marca de mouse de Computadora es:"+computadora1.getMarcaMouse());
-        System.out.println("Modelo de mouse de Computadora es:"+computadora1.getModeloMouse());
-        System.out.println("Marca de monitor de Computadora es:"+computadora1.getMarcaMmonitor());
-        System.out.println("Modelo de monitor de Computadora es:"+computadora1.getModeloMonitor());
-        System.out.println("Pulgadas de monitor de Computadora es:"+computadora1.getPulgadasMonitor());
-        System.out.println("Velocidad de procesador de Computadora es:"+computadora1.getVelocidadProcesador());
-        System.out.println("Tipo de mouse de Computadora es:"+computadora1.getTipoMouse());
-        System.out.println("Numero de teclas de Computadora es:"+computadora1.getNumeroTeclas());
-        System.out.println("Multimedia de Computadora es:"+computadora1.getMultimedia());
+        System.out.println("Marca de Computadora:"+computadora1.getMarca());
+        System.out.println("Modelo de Computadora:"+computadora1.getModelo());
+        System.out.println("Marca de teclado de Computadora:"+computadora1.getMarcaTeclado());
+        System.out.println("Modelo de teclado de Computadora:"+computadora1.getModeloTeclado());
+        System.out.println("Marca de mouse de Computadora:"+computadora1.getMarcaMouse());
+        System.out.println("Modelo de mouse de Computadora:"+computadora1.getModeloMouse());
+        System.out.println("Marca de monitor de Computadora:"+computadora1.getMarcaMmonitor());
+        System.out.println("Modelo de monitor de Computadora:"+computadora1.getModeloMonitor());
+        System.out.println("Pulgadas de monitor de Computadora:"+computadora1.getPulgadasMonitor());
+        System.out.println("Velocidad de procesador de Computadora:"+computadora1.getVelocidadProcesador());
+        System.out.println("Tipo de mouse de Computadora:"+computadora1.getTipoMouse());
+        System.out.println("Numero de teclas de Computadora:"+computadora1.getNumeroTeclas());
+        System.out.println("Multimedia de Computadora:"+computadora1.getMultimedia());
         
-        System.out.println("Marca de Monitor es:"+monitor1.getMarca());
-        System.out.println("Modelo de Monitor es:"+monitor1.getModelo());
-        System.out.println("pulgadas de Monitor es:"+monitor1.getPulgadas());
+        System.out.println("Marca de Monitor:"+monitor1.getMarca());
+        System.out.println("Modelo de Monitor:"+monitor1.getModelo());
+        System.out.println("pulgadas de Monitor:"+monitor1.getPulgadas());
         
-        System.out.println("Marca de Mouse es:"+mouse1.getMarca());
-        System.out.println("Modelo de Mouse es:"+mouse1.getModelo());
-        System.out.println("Tipo de Mouse es:"+mouse1.getTipo());
+        System.out.println("Marca de Mouse:"+mouse1.getMarca());
+        System.out.println("Modelo de Mouse:"+mouse1.getModelo());
+        System.out.println("Tipo de Mouse:"+mouse1.getTipo());
         
-        System.out.println("Marca de Teclado es:"+teclado1.getMarca());
-        System.out.println("Modelo de Teclado es:"+teclado1.getModelo());
-        System.out.println("numero de teclas de Teclado es:"+teclado1.getNumeroDeTeclas());
-        System.out.println("Multimedia de Teclado es:"+teclado1.getMultimedia());
+        System.out.println("Marca de Teclado:"+teclado1.getMarca());
+        System.out.println("Modelo de Teclado:"+teclado1.getModelo());
+        System.out.println("numero de teclas de Teclado:"+teclado1.getNumeroDeTeclas());
+        System.out.println("Multimedia de Teclado:"+teclado1.getMultimedia());
         
         
         
